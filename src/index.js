1dedfd4565e025c66924dd2a9734d9eef4d11c2a
Igor Shatrov <ihar.shatrov@gmail.com>
2020-09-29 15:59:23 +0300

    feat: create min func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    let min = Infinity
+    if (arguments.length === 0 || array[0] == undefined) {
+        return 0
+    }
+    array.forEach(element => {
+        if (element < min) {
+            min = element
+        }
+    });
+    return min
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return 0;
 }
